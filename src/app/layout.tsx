3c5354a193e0cd412491bf087665f9b19d433d77
Thomas Tang <hello@tangkunyin.com>
2024-08-25 19:53:55 +0800

    更新站点信息
    Signed-off-by: Thomas Tang <hello@tangkunyin.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,11 @@ import type { Metadata } from "next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "如何看待 - 问答就像与朋友聊天一样简单",
+  title: "如何看待 - 像朋友聊天一样愉快",
+  keywords: "聊天机器人，如何看待",
+  applicationName: "如何看待",
   description:
-    "一个神奇的网站，通过将现有文本文档、知识库转化成AI的记忆，使得问答、查找更智能和简单。",
+    "一个神奇的网站，如何看待将现有文档（PDF、TXT、Words、EPUB）提取并转录成AI知识库，通过对话形式交流原来需要主动阅读消化的信息，像与朋友聊天一样简单、愉快。",
 };
 
 export default function RootLayout({
