59fb6e71ed161fc832ec8ce4d77325cb448c7374
Thomas Tang <hello@tangkunyin.com>
2024-08-27 12:18:41 +0800

    首页基本样子有了
    Signed-off-by: Thomas Tang <hello@tangkunyin.com>

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,37 @@
+import {
+  FileSearchIcon,
+  MessageSquareText,
+  User,
+  Notebook,
+  PackageSearch,
+} from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
+const BookCard = () => {
+  return Array.from({ length: 30 }).map((_, index: number) => {
+    return (
+      <div className="rectangle bg-pink-500" key={`book_card_${index}`}>
+        {index < 10 ? `0${index + 1}` : index + 1}
+      </div>
+    );
+  });
+};
 
 export default function Home() {
   return (
     <div className="grid h-screen grid-rows-[5rem_1fr]">
-      <header className="sticky top-0 flex h-20 flex-row items-center justify-between bg-blue-200 px-10">
+      <header className="sticky top-0 flex h-20 flex-row items-center justify-between border-b bg-white px-10 shadow">
         <Avatar className="w-24">
           <AvatarImage src="logo.png" alt="@ruhekandai" />
           <AvatarFallback>LOGO</AvatarFallback>
@@ -13,39 +39,68 @@ export default function Home() {
 
         <div className="flex w-1/2 items-center space-x-2">
           <Input type="text" placeholder="搜的一下，你就知道" />
-          <Button type="submit">搜索</Button>
+          <Button type="submit">
+            <FileSearchIcon className="mr-2 h-4 w-4" /> 搜索
+          </Button>
         </div>
       </header>
 
       <div className="grid grid-cols-[12rem_1fr]">
-        <nav className="center w-48">
-          <ul className="grid w-full grid-rows-3 gap-5">
-            <li className="h-11 p-2">
-              <a href="#home" className="hover:text-white">
-                发现
-              </a>
+        <nav className="w-48">
+          <ul className="fixed top-36 grid w-48 grid-rows-4 gap-5">
+            <li className="center h-12 w-full">
+              <Button type="button">
+                <PackageSearch className="mr-2 h-4 w-4" /> 发现
+              </Button>
             </li>
-            <li className="h-11 p-2">
-              <a href="#about" className="hover:text-white">
-                发布
-              </a>
+            <li className="center h-12 w-full">
+              <Button type="button">
+                <Notebook className="mr-2 h-4 w-4" /> 发布
+              </Button>
             </li>
-            <li className="h-11 p-2">
-              <a href="#services" className="hover:text-white">
-                通知
-              </a>
+            <li className="center h-12 w-full">
+              <Button type="button">
+                <MessageSquareText className="mr-2 h-4 w-4" /> 通知
+              </Button>
             </li>
-            <li className="h-11 p-2">
-              <a href="#contact" className="hover:text-white">
-                个人
-              </a>
+            <li className="center h-12 w-full">
+              <Button type="button">
+                <User className="mr-2 h-4 w-4" /> 个人
+              </Button>
             </li>
           </ul>
         </nav>
 
         <main className="flex flex-col">
-          <div className="flex-1 bg-orange-200">这是信息流</div>
-          <footer className="center h-20 bg-gray-600">这是页脚</footer>
+          <div className="no-scrollbar grid flex-1 gap-4 overflow-y-scroll p-6 sm:grid-cols-3 xl:grid-cols-5">
+            {BookCard()}
+          </div>
+          <footer className="center h-20">
+            <Pagination>
+              <PaginationContent>
+                <PaginationItem>
+                  <PaginationPrevious href="#" />
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationLink href="#">1</PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationLink href="#" isActive>
+                    2
+                  </PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationLink href="#">3</PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationEllipsis />
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationNext href="#" />
+                </PaginationItem>
+              </PaginationContent>
+            </Pagination>
+          </footer>
         </main>
       </div>
     </div>
