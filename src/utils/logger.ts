7a2092859fe8e2b0a24bff426cad6a0d9ed4191d
Thomas Tang <hello@tangkunyin.com>
2024-11-06 13:20:05 +0800

    using curring to optimize logger
    Signed-off-by: Thomas Tang <hello@tangkunyin.com>

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,10 +2,11 @@ import chalk from 'chalk';
 
 import { isDevModel, getFullTime } from './common';
 
-function logWrapper(...args: any[]) {
-    if (isDevModel() && args.length > 1) {
-        const _log = (logs: any[]) => {
-            switch (args[0] as number) {
+function logWrapper(level: number) {
+    // Using curried wrapper
+    return (...logs: any[]) => {
+        if (isDevModel() && logs.length > 0) {
+            switch (level) {
                 case 1:
                     console.log(chalk.yellow.underline(`[${getFullTime()}]`), ...logs);
                     break;
@@ -16,19 +17,24 @@ function logWrapper(...args: any[]) {
                     console.log(chalk.green(`[${getFullTime()}]`), ...logs);
                     break;
             }
-        };
-        _log(args[1]);
-    }
+        }
+    };
 }
 
-export function logInfo(...args: any[]) {
-    logWrapper(0, args);
-}
+/**
+ * print standard console log with current time
+ * @param args log
+ */
+export const logInfo = (...args: any[]) => logWrapper(0)(...args);
 
-export function logWarn(...args: any[]) {
-    logWrapper(1, args);
-}
+/**
+ * print warning console log with current time
+ * @param args log
+ */
+export const logWarn = (...args: any[]) => logWrapper(1)(...args);
 
-export function logError(...args: any[]) {
-    logWrapper(-1, args);
-}
+/**
+ * print error console log with current time
+ * @param args log
+ */
+export const logError = (...args: any[]) => logWrapper(-1)(...args);
