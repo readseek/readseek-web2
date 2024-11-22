85c13bb4d4cae40d7e1cc706ee4ccbec413e7294
Thomas Tang <hello@tangkunyin.com>
2024-11-22 14:36:21 +0800

    动态获取分类和标签
    Signed-off-by: Thomas Tang <hello@tangkunyin.com>

--- src/app/(pages)/post/page.tsx
+++ src/app/(pages)/post/page.tsx
@@ -2,12 +2,10 @@
 
 import type { Document, Category, Tag } from '@/types';
 
-import { UploadCloud } from 'lucide-react';
 import { useEffect, useState, useCallback } from 'react';
 
 import { UploadBox } from '@/components/Chat/UploadBox';
 import { Button } from '@/components/ui/button';
-import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { doGet, doPost } from '@/utils/http';
 import { logError, logInfo, logWarn } from '@/utils/logger';
@@ -49,31 +47,41 @@ export default function PostContentPage() {
                 <UploadBox onFileSelected={onFileSelected} />
             </div>
 
-            <div className="flex h-24 w-1/3 items-center justify-start">
-                <Label htmlFor="picture" className="w-28">
-                    内容标签：
-                </Label>
+            <div className="flex h-24 w-1/3 items-center justify-around">
                 <Select>
                     <SelectTrigger className="w-52">
-                        <SelectValue placeholder="选择内容类型" />
+                        <SelectValue placeholder="请选择内容分类" />
                     </SelectTrigger>
                     <SelectContent>
                         <SelectGroup>
-                            <SelectItem value="auto">自动推断</SelectItem>
-                            <SelectItem value="news">时事新闻</SelectItem>
-                            <SelectItem value="technology">科学技术</SelectItem>
-                            <SelectItem value="history">历史人文</SelectItem>
-                            <SelectItem value="finance">金融理财</SelectItem>
-                            <SelectItem value="language">语言学习</SelectItem>
-                            <SelectItem value="essays">小说杂文</SelectItem>
+                            {categories.map((tag: Tag, index: number) => (
+                                <SelectItem key={`tg_${index}`} value={tag.value}>
+                                    {tag.key}
+                                </SelectItem>
+                            ))}
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+
+                <Select>
+                    <SelectTrigger className="w-52">
+                        <SelectValue placeholder="请选择内容标签" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            {tags.map((tag: Tag, index: number) => (
+                                <SelectItem key={`tg_${index}`} value={tag.value}>
+                                    {tag.key}
+                                </SelectItem>
+                            ))}
                         </SelectGroup>
                     </SelectContent>
                 </Select>
             </div>
 
-            <div className="flex h-24 w-1/3 items-center justify-start">
-                <Button type="submit">
-                    <UploadCloud className="mr-2 h-4 w-4" /> 发布
+            <div className="flex h-24 w-1/3 items-center justify-center">
+                <Button type="submit" className="mr-2 h-11 w-1/3">
+                    提交
                 </Button>
             </div>
         </main>
