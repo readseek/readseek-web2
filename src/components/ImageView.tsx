f810aa285d4017da6e8137c40f52f2fa27c10b36
Thomas Tang <hello@tangkunyin.com>
2024-12-25 20:34:58 +0800

    优化了一下公共过渡组件文案提醒和样式
    Signed-off-by: Thomas Tang <hello@tangkunyin.com>

--- src/components/ImageView.tsx
+++ src/components/ImageView.tsx
@@ -53,47 +53,47 @@ export function AvatarDefault({ male = true }: { male?: boolean }) {
     return <LocalImage file={male ? 'profile_male' : 'profile_female'} />;
 }
 
-export function NodataImage() {
+export function NodataImage({ message = '很抱歉，暂未获取到数据' }: { message?: string }) {
     return (
         <div className="h-64 w-1/6">
             <LocalImage file="no_data" />
-            <h3 className="mt-7 w-full text-center text-lg italic leading-10 text-slate-500">NO DATA FOUND</h3>
+            <h3 className="mt-7 w-full text-center text-base leading-10 text-slate-500">{message}</h3>
         </div>
     );
 }
 
-export function LoadingImage({ message = 'data is loading...' }: { message?: string }) {
-    return (
-        <div className="h-64 w-1/6">
-            <LocalImage file="loading" />
-            <h3 className="mt-7 w-full text-center text-lg italic leading-10 text-slate-500">{message}</h3>
-        </div>
-    );
-}
-
-export function ErrorImage({ message = 'error happened...' }: { message?: string }) {
-    return (
-        <div className="h-64 w-1/6">
-            <LocalImage file="50X" />
-            <h3 className="mt-7 w-full text-center text-lg italic leading-10 text-slate-500">{message}</h3>
-        </div>
-    );
-}
-
-export function SuccessImage({ message = 'congratulations!' }: { message?: string }) {
+export function SuccessImage({ message = '恭喜你，操作成功！' }: { message?: string }) {
     return (
         <div className="h-64 w-1/6">
             <LocalImage file="success" />
-            <h3 className="mt-7 w-full text-center text-lg italic leading-10 text-slate-500">{message}</h3>
+            <h3 className="mt-7 w-full text-center text-base leading-10 text-slate-500">{message}</h3>
         </div>
     );
 }
 
-export function WarnImage({ message = 'Oh, no...' }: { message?: string }) {
+export function LoadingImage({ message = '正在准备数据，请稍候...' }: { message?: string }) {
+    return (
+        <div className="h-64 w-1/6">
+            <LocalImage file="loading" />
+            <h3 className="mt-7 w-full text-center text-base leading-10 text-slate-500">{message}</h3>
+        </div>
+    );
+}
+
+export function ErrorImage({ message = '很不幸，服务器异常...' }: { message?: string }) {
+    return (
+        <div className="h-64 w-1/6">
+            <LocalImage file="50X" />
+            <h3 className="mt-7 w-full text-center text-base leading-10 text-slate-500 underline">{message}</h3>
+        </div>
+    );
+}
+
+export function WarnImage({ message = '请注意，即将有事发生...' }: { message?: string }) {
     return (
         <div className="h-64 w-1/6">
             <LocalImage file="warning" />
-            <h3 className="mt-7 w-full text-center text-lg italic leading-10 text-slate-500">{message}</h3>
+            <h3 className="mt-7 w-full text-center text-base leading-10 text-slate-500 underline">{message}</h3>
         </div>
     );
 }
