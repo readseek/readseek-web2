621c0d973e5f1a7693fb2afd5153881dce1613ad
Thomas Tang <hello@tangkunyin.com>
2025-01-02 15:46:32 +0800

    加载文档和embedding分别打点计时
    Signed-off-by: Thomas Tang <hello@tangkunyin.com>

--- src/utils/langchain/parser.ts
+++ src/utils/langchain/parser.ts
@@ -40,51 +40,55 @@ export type ParsedResult = {
 };
 
 export async function getSplitContents(filepath: string, extName: string) {
-    try {
-        console.time('🏆 Document Loading&splitting:');
-        const docs: Document[] = await getDocumentLoader(filepath, extName).load();
+    console.time('📊 loadAndTokenTextSplit costs:');
+    const docs: Document[] = await getDocumentLoader(filepath, extName).load();
+    const splitDocs = new TokenTextSplitter({
+        chunkSize: 4096, // 4k
+        chunkOverlap: 200,
+    }).splitDocuments(docs);
+    console.timeEnd('📊 loadAndTokenTextSplit costs:');
+    return splitDocs;
+}
 
-        return new TokenTextSplitter({
-            chunkSize: 4096, // 4k
-            chunkOverlap: 200,
-        }).splitDocuments(docs);
+export async function parseFileContent(filePath: string, extName: string): Promise<ParsedResult> {
+    try {
+        console.time('🕰 parseFileContent costs:');
+        const segments = (await getSplitContents(filePath, extName)) as LSegment[];
+        if (Array.isArray(segments) && segments.length > 0) {
+            // 标题和描述暂时均从第一节内容截取
+            const firstParts = segments[0].pageContent.split('\n\n');
+            logInfo('File firstPart:\n', firstParts);
+
+            let title = '',
+                description = '',
+                keywords = [''];
+            if (firstParts.length > 0) {
+                title = firstParts[0].replace(/(#|\*|%|@|\$|&|-{2,})/g, '').substring(0, 128);
+                description = firstParts
+                    .join('')
+                    .replace(/(#|\*|%|@|\$|&|-{2,})/g, '')
+                    .substring(0, 255);
+                // 暂时直接切割标题
+                keywords = title.split(' ');
+            }
+            // 返回实际的内容数据落库，以便前后台给用户展示
+            return {
+                state: true,
+                meta: {
+                    title,
+                    description,
+                    keywords,
+                    authors: ['tomartisan'], // 先写死，后面从前端传过来。或者从网络抓取
+                    coverUrl: process.env.__RSN_DEFAULT_COVER, // 后边从网络抓取，或随机
+                },
+                segments,
+            };
+        }
+        return { state: false };
     } catch (error) {
         logError('getSplitContents', error);
         throw error;
     } finally {
-        console.timeEnd('🏆 Document Loading&splitting:');
+        console.timeEnd('🕰 parseFileContent costs:');
     }
-}
-
-export async function parseFileContent(filePath: string, extName: string): Promise<ParsedResult> {
-    const segments = (await getSplitContents(filePath, extName)) as LSegment[];
-    if (Array.isArray(segments) && segments.length > 0) {
-        // 标题和描述暂时均从第一节内容截取
-        const firstParts = segments[0].pageContent.split('\n\n');
-        let title = '',
-            description = '',
-            keywords = [''];
-        if (firstParts.length > 0) {
-            title = firstParts[0].replace(/(#|\*|%|@|\$|&|-{2,})/g, '').substring(0, 128);
-            description = firstParts
-                .join('')
-                .replace(/(#|\*|%|@|\$|&|-{2,})/g, '')
-                .substring(0, 255);
-            // 暂时直接切割标题
-            keywords = title.split(' ');
-        }
-        // 返回实际的内容数据落库，以便前后台给用户展示
-        return {
-            state: true,
-            meta: {
-                title,
-                description,
-                keywords,
-                authors: ['tomartisan'], // 先写死，后面从前端传过来。或者从网络抓取
-                coverUrl: process.env.__RSN_DEFAULT_COVER, // 后边从网络抓取，或随机
-            },
-            segments,
-        };
-    }
-    return { state: false };
 }
