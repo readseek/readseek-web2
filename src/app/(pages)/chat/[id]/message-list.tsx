2dc59f9dceff3863dba51e0f7adb7b177b6e03b8
Thomas Tang <hello@tangkunyin.com>
2025-01-08 12:10:07 +0800

    nr
    Signed-off-by: Thomas Tang <hello@tangkunyin.com>

--- src/app/(pages)/chat/[id]/message-list.tsx
+++ src/app/(pages)/chat/[id]/message-list.tsx
@@ -1,4 +1,5 @@
 import { LocalImage } from '@/components/ImageView';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { MessageType, MessageStatus, buildMessage, Message } from '@/models/Message';
 
 interface MsgListProps {
@@ -17,18 +18,33 @@ export function MessageList({ data, onFeedback }: MsgListProps) {
     }
 
     return (
-        <div className="no-scrollbar my-5 w-[75%] overflow-y-scroll">
+        <div className="no-scrollbar my-5 w-[70%] overflow-y-scroll">
             {data.map((m: Message, i: number) => {
                 if (m.type === MessageType.In) {
                     return (
-                        <div key={`in_${m.id}#${i}`} className="rounded-md bg-gray-100 p-4 text-right">
-                            <span className="text-black">{m.text}</span>
+                        <div key={`in_${m.id}#${i}`} className="mb-3 flex w-full flex-row items-center justify-end">
+                            <div className="rounded-md bg-blue-50 px-2 pt-1 text-right">
+                                <div className="text-base text-black">{m.text}</div>
+                                <div className="mt-2 text-xs font-light italic">{new Date(m.timestamp).toLocaleString()}</div>
+                            </div>
+                            <div className="flex flex-col items-center">
+                                <Avatar className="ml-4">
+                                    <AvatarImage src={'https://tangkunyin.com/images/avatar.webp'} alt={`@user`} />
+                                    <AvatarFallback>avatar</AvatarFallback>
+                                </Avatar>
+                            </div>
                         </div>
                     );
                 }
                 return (
-                    <div key={`out_${m.id}#${i}`} className="my-3 rounded-md bg-gray-200 p-4 text-left">
-                        <span className="text-black">{m.text}</span>
+                    <div key={`out_${m.id}#${i}`} className="mb-3 flex flex-row items-center justify-start" style={{ width: 'calc(100% - 3.5rem)' }}>
+                        <div className="mr-4 aspect-square h-10 w-10">
+                            <LocalImage file="conference_speaker" />
+                        </div>
+                        <div className="w-full rounded-md bg-gray-100 px-2 pt-1 text-left">
+                            <div className="text-base text-black">{m.text}</div>
+                            <div className="mt-2 text-xs font-light italic">{new Date(m.timestamp).toLocaleString()}</div>
+                        </div>
                     </div>
                 );
             })}
